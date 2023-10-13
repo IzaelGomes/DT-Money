7554b71d27594eee3459a74e1f1c5e49aa83fa4e
izael gomes <izaelgomes127@gmail.com>
2023-10-13 18:34:48 -0300

    feat: component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-        <p>hello world</p>
+      <Transactions/>
    
 
     </ThemeProvider>
---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header";
+
+export function Transactions() {
+    return (
+    
+    <div>
+        <Header/>
+    </div>
+    
+)
+}
