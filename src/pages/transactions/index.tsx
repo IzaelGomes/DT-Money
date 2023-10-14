901811aab76ccba781702b71a7b574dd4dfa0fef
izael gomes <izaelgomes127@gmail.com>
2023-10-13 21:16:18 -0300

    feat: table

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,13 +1,38 @@
 import { Header } from "../../components/header";
 import { Summary } from "./components/Summary";
+import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 
 export function Transactions() {
-    return (
-    
+  return (
     <div>
-        <Header/>
-        <Summary/>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td> 13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td><PriceHighlight variant='outcome'> - R$ 12.000,00 </PriceHighlight></td>
+              <td>Venda</td>
+              <td> 13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td> 13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
     </div>
-    
-)
+  );
 }
